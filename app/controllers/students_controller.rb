236f80f273670eda05d3594c8d2bd8f9799b2253
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-19 12:30:54 -0400

    commit tree f846a109b7f9d85a1e7afa8bb51d9f673ad78dc5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,10 @@
 class StudentsController < ApplicationController
   
-  def index
-    @students = Student.all
-  end
 
+  def index
+    @students = Student.all 
+  end
+  
   def show
     @student = Student.find(params[:id])
   end
@@ -12,6 +13,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
